f2aad850ab2782a9126e6fc0b7e958f5c51818dd
te9no <tatuys9428@gmail.com>
2026-01-20 10:03:10 +0900

    fix

--- src/keyboard/Keyboard.tsx
+++ src/keyboard/Keyboard.tsx
@@ -622,7 +622,7 @@ export default function Keyboard() {
   }, [keymap, selectedLayerIndex]);
 
   return (
-    <div className="grid grid-cols-[auto_1fr] grid-rows-[1fr_32rem] bg-base-300 max-w-full min-w-0 min-h-0">
+    <div className="grid grid-cols-[auto_1fr] grid-rows-[1fr_28rem] bg-base-300 max-w-full min-w-0 min-h-0">
       <div className="p-2 flex flex-col gap-2 bg-base-200 row-span-2">
         {layouts && (
           <div className="col-start-3 row-start-1 row-end-2">
